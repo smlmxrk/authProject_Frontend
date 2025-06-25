bab0925dc733dadd5a4b9978a6eb88bfc6d64c5e
sml.mxrk <marktheartest@gmail.com>
2025-06-24 23:13:28 -0500

    next installation

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next.js';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     res.status(200).json({
